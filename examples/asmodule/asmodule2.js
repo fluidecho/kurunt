f1c392f16f8d55d413fef0332d5ca95461c1f4bb
markcode <mark@kurunt.com>
2014-01-08 11:46:57 +1100

    dev

--- examples/asmodule/asmodule2.js
+++ examples/asmodule/asmodule2.js
@@ -1,5 +1,5 @@
 //
-// Kurunt, As Module
+// Kurunt, As Module 2
 //
 // Using Kurunt as a module framework, rather than stand-alone.
 // Version: 0.2
@@ -18,6 +18,7 @@ var topology      = require("../.././topology.json");
 
 var workers       = {};
 workers.myworker  = __dirname + '/myworker.js';     // full path to your worker function.
+workers.myworker2  = __dirname + '/myworker2.js';   // full path to your worker function.
 
 var stores        = {};
 stores.mystore    = __dirname + '/mystore.js';      // full path to your store function.
@@ -50,5 +51,23 @@ Kurunt.init(config, topology, workers, stores, function(kurunt) {
 
   });
   
+  // can send another message to another stream, using myworker2.
+  kurunt.newStream('http', 'myworker2', use_stores, tags, [], function(stream) {
+
+    // can now form and send my message into the stream. There are lots of ways you can input data: http://docs.kurunt.com/Input_Data.
+    var mymessage = {};
+    mymessage.foo = 'bar';
+    mymessage.int = 9876543210;
+    mymessage.fab = true;
+
+    // will send this message in JSON, as that is the format myworker.js is expecting, could use any message format matching worker.
+    kurunt.send(stream, JSON.stringify(mymessage), function (e, sent) {
+      console.log('asmodule.js> Sent message: ' + sent + ', mymessage: ' + JSON.stringify(mymessage));
+      //kurunt.exit();    // can exit all kurunt processes (as set within topology) when has had time to complete message processing.
+      console.log('asmodule.js> Can view processed message at: http://127.0.0.1:9001/.');   // requires socket.io.
+    });
+
+  });  
+
 });
 
---
+++ examples/asmodule/myworker2.js
@@ -0,0 +1,61 @@
+//
+// Kurunt, My Worker 2
+//
+// Using Kurunt as a module framework, My Worker 2.
+// Version: 0.2
+// Author: Mark W. B. Ashcroft (mark [at] kurunt [dot] com)
+// License: MIT or Apache 2.0.
+//
+// Copyright (c) 2013-2014 Mark W. B. Ashcroft.
+// Copyright (c) 2013-2014 Kurunt.
+//
+
+
+
+// must export 'work' module.
+module.exports.work = function (message, wk, fn, callback) {
+  // use try catch so can skip over invalid messages.
+  try {
+
+    console.log('myworker2@workers> MESSAGE: ' + require('util').inspect(message, true, 99, true));    // uncomment to debug message.
+    
+    // Can process the message anyway you want, use: functions, parse, regex, filter, augment, geoip, etc.
+    
+    var mymessage = JSON.parse( message.message.toString(wk['config']['encoding']) );   // example for JSON formatted data.
+    //console.log('myworker2@workers> mymessage: ' + require('util').inspect(mymessage, true, 99, true));    // uncomment to debug message.
+    
+    // Can set the attributes, as they match with: config.stores.mystore.schema.
+    var attributes = [];
+    attributes['mymessage'] = mymessage;
+
+    return callback( [ message, attributes ] );   // must return.
+  
+  } catch(e) {
+    //console.log('myworker2@workers> ERROR: ' + require('util').inspect(e, true, 99, true));     // uncomment to debug errors.
+    return callback( false );   // must return.
+  }
+};
+
+
+// set the worker config, or call a json config file via require.
+var config = {
+  "name": "myworker2",
+  "title": "My Worker 2", 
+  "description": "Using Kurunt as a module framework, My Worker 2.",
+  "inputs": [ "tcp", "udp", "http" ],
+  "mq_nodelay": false,
+  "reports": [ "stream" ],
+  "message_codec": "json",
+  "encoding": "utf8",
+  "stores": [
+    {
+      "mystore": {
+        "schema": {
+          "mymessage": { }
+        }
+      }
+    } 
+  ]
+};
+exports.config = config;    // must export the config so kurunt can read it.
+
